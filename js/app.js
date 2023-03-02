ebd9855ce224eb1bed0fd865c0f56f453e0a3e61
Shefat Tonmoy <shefathossain7@gmail.com>
2023-03-03 03:21:05 +0600

    spinner done

--- js/app.js
+++ js/app.js
@@ -1,29 +1,45 @@
-const fetchAllData = () => {
-    fetch("https://openapi.programming-hero.com/api/ai/tools")
-        .then((response) => response.json())
-        .then((data) => showAllData(data.data));
-};
+const fetchAllData = async() => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const response = await fetch(url);
+    const data = await response.json();
+    showAllData(data.data);
+}
 
-fetchAllData();
+
 
 const showAllData = (data) => {
-    console.log(data);
     const cardContainer = document.getElementById("card-container");
-    data.tools.forEach((singleData) => {
-        const cardDiv = document.createElement("div");
-        cardDiv.classList.add("col");
+    data.tools.slice(0, 6).forEach((singleData) => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
         cardDiv.innerHTML = `
-        <div class="card">
+        <div class="card p-4 h-100">
                     <img src="${singleData.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <h5 class="card-title">Features</h5>
+                      <p class="card-text">${singleData.features}</p>
                     </div>
                     <div class="card-footer">
+                    <h5 class="card-title">${singleData.name}</h5>
                       <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
         </div>
         `;
         cardContainer.appendChild(cardDiv);
     });
-};
+}
+
+
+fetchAllData();
+
+// Spinner Section
+
+const spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', () =>{
+  spinnerWrapper.style.opacity = '0';
+
+  setTimeout(() =>{
+    spinnerWrapper.style.display = 'none';
+  }, 200);
+})
